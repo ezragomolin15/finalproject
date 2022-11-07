01bfe27ada90b23c86ecd47a14915ebe17968229
Ezra <59707452+ezragomolin15@users.noreply.github.com>
2022-11-07 12:11:30 -0500

    first commit

---
+++ src/main/java/ecse437/App.java
@@ -0,0 +1,13 @@
+package ecse437;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
